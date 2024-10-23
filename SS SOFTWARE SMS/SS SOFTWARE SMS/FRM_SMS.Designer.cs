79e5a2b869f7b4219fa3d78792ff400c2613c3bf
Harshit Jain <harshitostwal1234@gmail.com>
2024-10-23 12:47:58 +0530

    Add project files.

---
+++ SS SOFTWARE SMS/SS SOFTWARE SMS/FRM_SMS.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace SS_SOFTWARE_SMS
+{
+    partial class FRM_SMS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbcom = new System.Windows.Forms.ComboBox();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.txtmessage = new System.Windows.Forms.TextBox();
+            this.btnsend = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbcom
+            // 
+            this.cmbcom.FormattingEnabled = true;
+            this.cmbcom.Location = new System.Drawing.Point(376, 102);
+            this.cmbcom.Name = "cmbcom";
+            this.cmbcom.Size = new System.Drawing.Size(121, 21);
+            this.cmbcom.TabIndex = 0;
+            this.cmbcom.SelectedIndexChanged += new System.EventHandler(this.cmbcom_SelectedIndexChanged);
+            // 
+            // txtphone
+            // 
+            this.txtphone.Location = new System.Drawing.Point(291, 210);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(284, 20);
+            this.txtphone.TabIndex = 1;
+            // 
+            // txtmessage
+            // 
+            this.txtmessage.Location = new System.Drawing.Point(291, 236);
+            this.txtmessage.Name = "txtmessage";
+            this.txtmessage.Size = new System.Drawing.Size(284, 20);
+            this.txtmessage.TabIndex = 2;
+            // 
+            // btnsend
+            // 
+            this.btnsend.Location = new System.Drawing.Point(291, 275);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(284, 48);
+            this.btnsend.TabIndex = 3;
+            this.btnsend.Text = "button1";
+            this.btnsend.UseVisualStyleBackColor = true;
+            this.btnsend.Click += new System.EventHandler(this.btnsend_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FRM_SMS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnsend);
+            this.Controls.Add(this.txtmessage);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.cmbcom);
+            this.Name = "FRM_SMS";
+            this.Text = "FRM_SMS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FRM_SMS_FormClosing);
+            this.Load += new System.EventHandler(this.FRM_SMS_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbcom;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.TextBox txtmessage;
+        private System.Windows.Forms.Button btnsend;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ SS SOFTWARE SMS/SS SOFTWARE SMS/FRM_SMS.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace SS_SOFTWARE_SMS
+{
+    public partial class FRM_SMS : Form
+    {
+        private SerialPort serialPort;
+
+        public FRM_SMS()
+        {
+            InitializeComponent();
+
+            // Initialize the SerialPort
+            serialPort = new SerialPort();
+
+            // Populate the combo box with available serial ports
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cmbcom.Items.Add(port);
+            }
+
+            // Select the first port if available
+            if (ports.Length > 0)
+            {
+                cmbcom.SelectedIndex = 0;
+            }
+        }
+
+        private void FRM_SMS_Load(object sender, EventArgs e)
+        {
+            // Disable the Send button until a port is selected
+            btnsend.Enabled = false;
+        }
+
+        private void FRM_SMS_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Close the serial port when the form is closing
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        private void btnsend_Click(object sender, EventArgs e)
+        {
+            string phoneNumber = txtphone.Text;
+            string message = txtmessage.Text;
+
+            try
+            {
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.Open();
+                }
+
+                serialPort.WriteLine("AT+CMGF=1" + Environment.NewLine);
+                string response = serialPort.ReadExisting();
+                Console.WriteLine("Response: " + response); // Log response
+
+                // Set the recipient's phone number
+                serialPort.WriteLine($"AT+CMGS=\"{phoneNumber}\"" + Environment.NewLine);
+                System.Threading.Thread.Sleep(100);
+                response = serialPort.ReadExisting();
+                Console.WriteLine("Response: " + response); // Log response
+
+                // Write the message and control character (Ctrl+Z) to send
+                serialPort.Write(message + (char)26);
+                serialPort.BaseStream.Flush();
+                System.Threading.Thread.Sleep(3000); // Wait for the message to be sent
+                response = serialPort.ReadExisting();
+                Console.WriteLine("Response: " + response); // Log response
+
+                if (response.Contains("ERROR"))
+                {
+                    MessageBox.Show($"Error in sending SMS: {response}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("SMS Sent Successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error sending SMS: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close(); // Close the port after sending
+                }
+            }
+        }
+
+
+        private void cmbcom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Update the selected port when the combo box selection changes
+            serialPort.PortName = cmbcom.SelectedItem.ToString();
+            btnsend.Enabled = true; // Enable the Send button when a port is selected
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Refresh the COM port list
+            cmbcom.Items.Clear(); // Clear the existing items
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cmbcom.Items.Add(port);
+            }
+
+            // Select the first port if available
+            if (cmbcom.Items.Count > 0)
+            {
+                cmbcom.SelectedIndex = 0;
+            }
+        }
+    }
+}
